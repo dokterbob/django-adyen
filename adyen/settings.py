75f345f2236e8dafb68da4bd2dd4286afdd27fa5
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-07-06 16:58:37 +0200

    Settings

--- adyen/settings.py
+++ adyen/settings.py
@@ -1,3 +1,10 @@
 import logging
 logger = logging.getLogger(__name__)
 
+from django.conf import settings
+
+
+MERCHANT_ACCOUNT = getattr(settings, 'ADYEN_MERCHANT_ACCOUNT')
+MERCHANT_SECRET = getattr(settings, 'ADYEN_MERCHANT_SECRET')
+
+DEBUG = getattr(settings, 'ADYEN_DEBUG', True)
