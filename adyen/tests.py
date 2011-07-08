49d32de5fd47a9cf87ecfde897a3153f8d6dd81b
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-07-08 12:38:03 +0200

    Skeleton for test code.

--- adyen/tests.py
+++ adyen/tests.py
@@ -1,19 +1,24 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
 import logging
 logger = logging.getLogger(__name__)
-
 
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class SessionTest(TestCase):
+    """ Test initiating payments. """
+
+    def test_signature(self):
+        """ Test the signature against Adyen's online signature tester. """
+        pass
+
+    def test_validation(self):
+        """ Create a signature and check whether validation succeeds. """
+        pass
+
+    def test_session(self):
+        """ Test whether initiating a payment session works. """
+        pass
+
+    def test_result(self):
+        """ Test whether a result call gets processed in the right way. """
+        pass
