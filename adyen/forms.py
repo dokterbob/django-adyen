e214d8b9e69b34cbcba0c95b212f083f90ea6778
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-07-09 17:03:25 +0200

    More specific Adyen form code and settings.

--- adyen/forms.py
+++ adyen/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 
+from adyen.interface import PaymentInterface
+from adyen import settings
+
+
 class HiddinInputForm(forms.Form):
     """ Form which by default renders all initial_data as hidden fields. """
 
@@ -9,3 +13,31 @@ class HiddinInputForm(forms.Form):
         for name, value in initial_data.items():
             self.fields[name] = forms.CharField(widget=forms.HiddenInput,
                 initial=value)
+
+
+class AdyenForm(HiddinInputForm):
+    """
+    Subclass of the HiddenInputForm, making sure stuff like signatures are
+    set properly after initialization of the variables and also setting
+    some fields from Django settings.
+    """
+
+    def __init__(self, initial_data, *args, **kwargs):
+        # Set some defaults from the settings
+        new_data = {'skinCode': settings.DEFAULT_SKIN,
+                    'merchantAccount': settings.MERCHANT_ACCOUNT}
+
+        # Always allow for overriding of the defaults with custom data
+        new_data.update(initial_data)
+
+        # Create an interface and sign the data - this updates the data
+        # in-place
+        self.interface = PaymentInterface(new_data,
+                                          settings.MERCHANT_SECRET,
+                                          testing=settings.DEBUG,
+                                          onepage=settings.ONE_PAGE)
+        self.interfaec.sign()
+
+        super(AdyenForm, self).__init__(new_data, *args, **kwargs)
+
+
--- adyen/settings.py
+++ adyen/settings.py
@@ -3,8 +3,11 @@ logger = logging.getLogger(__name__)
 
 from django.conf import settings
 
-
+# Required settings
 MERCHANT_ACCOUNT = getattr(settings, 'ADYEN_MERCHANT_ACCOUNT')
 MERCHANT_SECRET = getattr(settings, 'ADYEN_MERCHANT_SECRET')
 
+# Optional settings
 DEBUG = getattr(settings, 'ADYEN_DEBUG', True)
+DEFAULT_SKIN = getattr(settings, 'ADYEN_DEFAULT_SKIN', None)
+ONE_PAGE = getattr(settings, 'ADYEN_ONE_PAGE', True)
